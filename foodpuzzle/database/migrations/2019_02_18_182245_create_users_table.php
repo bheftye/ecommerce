d0f44abecbc834dc270a409e50be4504112d7142
Lulu Chang <luluchang@Lulude-MacBook-Air.local>
2019-02-19 12:33:38 +0100

    migrations

---
+++ foodpuzzle/database/migrations/2019_02_18_182245_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('user_autonmy');
+            $table->string('username');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
