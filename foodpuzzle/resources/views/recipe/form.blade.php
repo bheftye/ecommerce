f0771f3cb453b8917c3ad20b94bd3b9adbfdc772
Brent Heftye <bheftye92@gmail.com>
2019-02-26 16:08:18 +0100

    Recipe in two columns.

--- foodpuzzle/resources/views/recipe/form.blade.php
+++ foodpuzzle/resources/views/recipe/form.blade.php
@@ -1,56 +1,59 @@
 <div class="col-12 pt-5">
-    <form id="recipe-form" method="POST" action="/recipe/create">
-        @csrf
-        <div class="form-group">
-            <div class="input-group">
-                <input class="form-control" type="text" name="name" placeholder="Recipe's name"/>
+    <form id="recipe-form" class="row" method="POST" action="/recipe/create">
+        <div class="col-12 col-md-6">
+            @csrf
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" type="text" name="name" placeholder="Recipe's name"/>
+                </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <div class="input-group">
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <textarea class="form-control" name="steps" placeholder="Recipe's steps"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" type="number" name="calories" placeholder="Calories"/>
                 </div>
             </div>
         </div>
+        <div class="col-12 col-md-6">
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" type="number" name="fat" placeholder=" % Fat"/>
+                </div>
+            </div>
 
-        <div class="form-group">
-            <div class="input-group">
-                <textarea class="form-control" name="steps" placeholder="Recipe's steps"></textarea>
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" type="number" name="carbohydrate" placeholder="Carbohydrates"/>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" type="number" name="protein" placeholder=" % Protein"/>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-group">
+                    <input class="form-control" type="number" name="sugar" placeholder="Sugar"/>
+                </div>
             </div>
         </div>
-
-        <div class="form-group">
-            <div class="input-group">
-                <input class="form-control" type="number" name="calories" placeholder="Calories"/>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="input-group">
-                <input class="form-control" type="number" name="fat" placeholder=" % Fat"/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="input-group">
-                <input class="form-control" type="number" name="carbohydrate" placeholder="Carbohydrates"/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="input-group">
-                <input class="form-control" type="number" name="protein" placeholder=" % Protein"/>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="input-group">
-                <input class="form-control" type="number" name="sugar" placeholder="Sugar"/>
-            </div>
-        </div>
-
         <button type="submit" class="btn btn-success btn-lg btn-block">Submit</button>
     </form>
 </div>
