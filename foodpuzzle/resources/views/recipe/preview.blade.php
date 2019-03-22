bee436a433b872ec22d1dad96c8a7010e6c7bc53
Brent Heftye <bheftye92@gmail.com>
2019-03-22 11:33:16 +0100

    Buttons for Delete and Edit.

--- foodpuzzle/resources/views/recipe/preview.blade.php
+++ foodpuzzle/resources/views/recipe/preview.blade.php
@@ -11,10 +11,12 @@ $fileName = $filePathInParts[1];
 /* Check if the current recipe is favorite from logged in user. */
 $isFavorite = false;
 $isUser = false;
+$isOwner = false;
 if (Auth::check()){
     $isUser = true;
     $user = Auth::user();
     $isFavorite = Favorite::isFavorite($recipe->id, $user->id);
+    $isOwner = $recipe->u_id === $user->id;
 }
 @endphp
 <div class="col-12 col-md-4 mt-3">
@@ -24,6 +26,14 @@ if (Auth::check()){
         <a href="/recipe/favorite/{{$recipe->uuid}}" class="heart">
             <i style="color:red" class="{{$isFavorite? "fas fa-heart" : "far fa-heart"}}"></i>
         </a>
+        @endif
+        @if ($isOwner && $showConfiguration)
+            <a href="/recipe/delete/{{$recipe->uuid}}" class="trash" title="Delete">
+                <i class="fas fa-trash-alt"></i>
+            </a>
+            <a href="/recipe/edit/{{$recipe->uuid}}" class="edit" title="Edit">
+                <i class="fas fa-pencil-alt"></i>
+            </a>
         @endif
         <div class="card-body">
             <a href="/recipe/{{$recipe->uuid}}" class="link">
@@ -38,4 +48,8 @@ if (Auth::check()){
     a.heart {display:block;color:red;background-color: #ffffff;border-radius: 50%;float: right;font-size:25px;position:absolute;right:-10px;top:-10px;height:40px;width:40px;}
     a.heart i {margin-left:8px;margin-top:8px;}
     .card .card-img-top{max-height:200px !important;}
+    a.trash, a.edit{display:block;background-color:#ffffff;border-radius: 50%;font-size:18px;position:absolute;height:30px;width:30px;}
+    a.trash{color:red;bottom:50px;right:5px;}
+    a.edit{color:blue;bottom:50px;right:40px;}
+    a.trash i, a.edit i{margin-left:7px;margin-top:5px;}
 </style>
--- foodpuzzle/resources/views/recipe/recipes.blade.php
+++ foodpuzzle/resources/views/recipe/recipes.blade.php
@@ -1,11 +1,17 @@
 @php
 $showFavorites = isset($showFavorites)? $showFavorites: true;
+$showConfiguration = isset($showConfiguration)? $showConfiguration: false;
 @endphp
 <div class="col-12 mt-5">
     <div class="row">
         @if (count($recipes) > 0)
             @foreach($recipes as $recipe)
-                @include('recipe.preview', ['recipe' => $recipe, 'showFavorites' => $showFavorites])
+                @include('recipe.preview',
+                [
+                 'recipe' => $recipe,
+                 'showFavorites' => $showFavorites,
+                 'showConfiguration' => $showConfiguration
+                 ])
             @endforeach
         @else
             <div class="col-md-12">
